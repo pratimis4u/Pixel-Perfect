7e07f1c63b773e2f771719cc48b872aa3289c719
pratimis4u <pratimis4u@gmail.com>
2024-11-28 09:24:24 +0530

    fixed bugs

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,29 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationPage from '../../[id]/page';
+import TransformationForm from '@/components/shared/TransformationForm';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({params:{type}}:
+  SearchParamProps) => {
+    const transformation = transformationTypes[type];
   return (
-    <div>
-      AddTransformationTypePage
-    </div>
+    <>
+    <Header title={transformation.title}
+    subtitle={transformation.subTitle} />
+
+    <TransformationPage />
+    {/* <section className="mt-10">
+      <TransformationForm
+      action="Add"
+      userId={user._id}
+      type={transformation.type as TransformationTypeKey}
+      creditBalance={userAgent.creditBalance}
+      />
+    </section> */}
+
+  
+</>
   )
 }
 
